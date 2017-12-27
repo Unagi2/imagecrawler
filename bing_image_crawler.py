30ead12dcc3e5dab50af5432df4ca8e272304932
My Name <my-name@chromium.org>
2017-12-27 22:37:02 +0900

    fix bing web search

--- bing_image_crawler.py
+++ bing_image_crawler.py
@@ -13,7 +13,7 @@ url = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
 # parameters
 query = "キーワード"
 count = 50      # 1リクエストあたりの最大取得件数 default:30 max:150
-mkt = "ja-JP"   # 取得元の国
+mkt = "ja-JP"   # 取得元の国コード
 
 num_per = 10    # リクエスト回数（count * num_per=取得画像数）
 offset = math.floor(count / num_per)    # ループ回数
@@ -29,9 +29,8 @@ for offset_num in range(offset):
     for values in data['value']:
         image_url = values['contentUrl']
         try:
-            imgutil.download_img(path,values['contentUrl'])
+            imgutil.download_img(path,image_url)
         except Exception as e:
             print("failed to download image at {}".format(image_url))
             print(e)
-            continue
     time.sleep(1)
